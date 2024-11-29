a96daf73ab06709794a526e1fea82b313de2c40e
Tirth-007 <barot.tirth0704@gmail.com>
2024-11-30 00:27:59 +0530

    A commit resolved from previous commit in index.php file of backend folder

--- Backend/index.php
+++ Backend/index.php
@@ -7,7 +7,7 @@ include ('connection.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Medical Store</title>
+    <title>Admin - MedPlus Pharmacy</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
