2e5e2a093fe06b9f36ab884b83fc087ae0b3621f
Tirth-007 <barot.tirth0704@gmail.com>
2024-12-03 12:30:26 +0530

    Refactoring managing inventory feature inserting category is done now product is remaining in add_product.php via manage_inventory.php in backend folder

--- Backend/admin_dashboard.php
+++ Backend/admin_dashboard.php
@@ -1,15 +1,6 @@
 <?php
     include('admin_session.php');
     include('connection.php');
-    
-    //Check if admin is logged in
-    // if(!isset($_SESSION['is_admin_logged_in']) || $_SESSION['is_admin_logged_in'] !==true){
-    //     echo "Admin is logged in. " . $_SESSION['admin_name'];
-    //     header('location: index.php?timeout=true');
-    //     exit();
-    // }
-
-    
 ?>
 
 <!DOCTYPE html>
