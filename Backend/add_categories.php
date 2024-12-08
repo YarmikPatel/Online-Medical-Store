b5ca8a462e3f292aef87c92f2211a7c6b045707e
YarmikPatel <kansagarayarmik@gmail.com>
2024-12-08 15:32:28 +0530

    Added new file add_categories.php and removed add_products.php in backend folder

---
+++ Backend/add_categories.php
@@ -0,0 +1,58 @@
+<?php
+    include('admin_session.php');
+    include('connection.php');
+
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $category_id = $conn->real_escape_string($_POST['category_id']);
+        $name = $conn->real_escape_string($_POST['name']);
+
+        //SQL query to post data into database.
+        $sql = "INSERT INTO `category` (`category_id`,`name`) VALUES ('$category_id','$name')";
+        $result = mysqli_query($conn,$sql);
+        //Verifying the data from database.
+        if($result){
+             // Get the last inserted ID
+            // $last_id = $conn->insert_id;
+
+            // Set session variables
+            $_SESSION['category_id'] = $category_id;
+            $_SESSION['name'] = $name;
+            // $_SESSION['user_email'] = $email;
+            echo "Category records inserted successfully";
+            header("location:save_product.php");
+        }
+        else{
+            echo "Records not inserted successfully";
+        }
+    }
+
+    $conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product - Category</title>
+</head>
+<body>
+    <div class="main">
+        <div class="form">
+            <form method="post">
+                <div class="inputBx" id="category_id">
+                    Enter category id <br>
+                    <input type="text" name="category_id" id="category_id">
+                </div>
+                <div class="inputBx" id="name">
+                    Enter category name <br>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div class="inputBX" id="submit">
+                    <input type="submit" value="Add category">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- Backend/manage_inventory.php
+++ Backend/manage_inventory.php
@@ -23,7 +23,8 @@
         <p>Manage Inventory</p>
     </div>
     <div class="main">
-        <div class="dashboard-items" id="mi"><a href="add_product.php">Add Product</a></div>
+    <div class="dashboard-items" id="mi"><a href="add_categories.php">Add Categories</a></div>
+        <div class="dashboard-items" id="mi"><a href="save_product.php">Add Product</a></div>
         <div class="dashboard-items" id="mu"><a href="view_products.php">View Product</a></div>
         <div class="dashboard-items" id="mo"><a href="update_product.php">Edit/Update Product</a></div>
         <div class="dashboard-items" id="rpt"><a href="delete_product.php">Delete Product</a></div>
