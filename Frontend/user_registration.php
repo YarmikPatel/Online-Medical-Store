70ddde4c6de394f66ee2c78b15777c993187da33
Tirth-007 <barot.tirth0704@gmail.com>
2024-12-16 22:43:05 +0530

    In frontend folder database logic of sign up is completed only js validation

--- Frontend/user_registration.php
+++ Frontend/user_registration.php
@@ -25,21 +25,24 @@ include('../Backend/connection.php');
                 Enter User Password:
                 <input type="password" id="upass" name="upass">
             </div>
-            <div class="inputBx" id="userpass">
+            <div class="inputBx" id="confirmuserpass">
                 Enter Confirm User Password:
-                <input type="password" id="upass" name="upass">
+                <input type="password" id="confirmupass" name="confirmupass">
             </div>
-            <div class="inputBx" id="userpass">
+            <div class="inputBx" id="usermobile">
                 Enter Mobile number:
-                <input type="text" id="upass" name="upass">
+                <input type="text" id="mobile" name="mobile">
             </div>
-            <div class="inputBx" id="userpass">
+            <div class="inputBx" id="useremailid">
                 Enter Email id:
-                <input type="text" id="upass" name="upass">
+                <input type="text" id="email_id" name="email_id">
             </div>
-            <div class="inputBx" id="userpass">
+            <div class="inputBx" id="useraddress">
                 Enter Address:
-                <input type="Textarea" id="upass" name="upass">
+                <input type="Textarea" id="address" name="address">
+            </div>
+            <div class="inputBx" id="signup">
+                <button type="submit">Sign Up</button>
             </div>
         </form>
     </div>
@@ -48,25 +51,37 @@ include('../Backend/connection.php');
 
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $admin_name = $_POST["admin_name"];
-    $admin_name = $_POST["admin_name"];
-    $admin_name = $_POST["admin_name"];
-    $admin_name = $_POST["admin_name"];
-    $apass = $_POST["apass"];
-    //SQL query to post data into database.
-    $sql = "Select * from admin_info where admin_name='$admin_name' AND apass='$apass'";
+    $uname = $_POST["uname"];
+    $full_name = $_POST["full_name"];
+    $upass = $_POST["upass"];
+    $mobile = $_POST["mobile"];
+    $email_id = $_POST["email_id"];
+    $address = $_POST["address"];
+    // Check the user name in the registration table if already taken
+    $sql = "Select * from registration where uname='$uname'";
     $result = mysqli_query($conn,$sql);
-    $num = mysqli_num_rows($result);
-    //Verifying the data from database.
-    if($num >= 1){
-        //Set session variables
-        // session_start();
-        // $_SESSION['admin_name']=$admin_name;
-        // $_SESSION['apass']=$apass;
-        header("location:admin_dashboard.php");
+    if(mysqli_num_rows($result) > 0){
+        echo "The username '$uname' is already taken. Please choose a different one.";
     }
     else{
-        echo "<script>alert('Invalid login credentials');</script>";
+            //SQL query to post data into database.
+            $sql = "INSERT INTO `registration` (`uname`,`full_name`,`upass`,`mobile`,`email_id`,`address`) VALUES   ('$uname','$full_name','$upass','$mobile','$email_id','$address')";
+            $result = mysqli_query($conn,$sql);
+        if($result){
+            //Set session variables
+            // session_start();
+            $_SESSION['uname']=$uname;
+            $_SESSION['full_name']=$full_name;
+            $_SESSION['upass']=$upass;
+            $_SESSION['mobile']=$mobile;
+            $_SESSION['email_id']=$email_id;
+            $_SESSION['address']=$address;
+            // header("location:admin_dashboard.php");
+            echo "<script>alert(Registration is successfully done);</script>";
+        }
+        else{
+            echo "<script>alert('Invalid login credentials');</script>";
+        }
     }
 }
 ?>
